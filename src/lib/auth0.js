c8773c3c21c4be02e6ec0cb76cc664e6e75f2ce0
eityans <eityans@gmail.com>
2025-06-19 19:44:32 +0900

    fix

--- src/lib/auth0.js
+++ src/lib/auth0.js
@@ -9,7 +9,7 @@ export const auth0 = new Auth0Client({
   domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
   clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
   clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET,
-  //appBaseUrl: process.env.NEXT_PUBLIC_APP_BASE_URL,
+  appBaseUrl: process.env.NEXT_PUBLIC_APP_BASE_URL,
   secret: process.env.NEXT_PUBLIC_AUTH0_SECRET,
 
   authorizationParameters: {
